5ca95d2983cbf20b426bfba9b25c5a56c766c875
HoangDSR <54090585+hoang3402@users.noreply.github.com>
2024-04-09 16:37:06 +0700

    Feat: login with github (#1)

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
+"use client";
+
+import { URL_API } from "./layout";
+
 export default function Home() {
-	return <></>;
+	function loginWithGithub() {
+		console.log("login with github");
+		window.location.href = `${URL_API}/api/connect/github`;
+	}
+
+	return (
+		<>
+			<div>
+				<button onClick={loginWithGithub}>Login with Github</button>
+			</div>
+		</>
+	);
 }
